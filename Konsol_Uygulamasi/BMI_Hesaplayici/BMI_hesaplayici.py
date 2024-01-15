4d60a272d37e1eaa1b1ac83e212b88d935ccb08c
ozmenmustafa06 <ozmenmustafa06@gmail.com>
2024-01-15 20:19:35 +0300

    Proje1

---
+++ Konsol_Uygulamasi/BMI_Hesaplayici/BMI_hesaplayici.py
@@ -0,0 +1,21 @@
+import sys
+import time
+
+def bmimenu():
+    print("╔════════════════════════════╗")
+    print("║      BMI HESAPLAYICI       ║")
+    print("║                            ║")
+    print("║ 1- BMI Nedir?              ║")
+    print("║ 2- BMI Ne İçin Hesaplanır? ║")
+    print("║ 3- BMI Nasıl Hesaplanır    ║")
+    print("║ 4- BMI Hesaplama           ║")
+    print("║ 5- Ana Menü                ║")
+    print("║ 6- Çıkış                   ║")
+    print("║                            ║")
+    print("╚════════════════════════════╝")
+    print("")
+
+
+
+
+bmimenu()
